ec60c32f57e0f666e230294c1c94de47afdfb3a4
soniabronner <148773001+soniabronner@users.noreply.github.com>
2026-01-03 14:57:43 +0100

    Update data_preparation_pipeline.py

--- src/utils/data_preparation_pipeline.py
+++ src/utils/data_preparation_pipeline.py
@@ -1,7 +1,18 @@
 from utils.data_utils import get_baseline_dataset, get_extended_dataset
-from utils.split_utils import train_test_split
 
+def data_preparation_pipeline(
+    df,
+    *,
+    extension: bool = False,
+    annotated: bool = True
+):
+    if extension:
+        X, y_dep, y_sen, meta = get_extended_dataset(df)
+    else:
+        X, y_dep, y_sen, meta = get_baseline_dataset(df)
 
-def data_preparation_pipeline():
-    pass
+    if not annotated:
+        y_dep = None
+        y_sen = None
 
+    return X, y_dep, y_sen, meta
