ce9a3bf2e51e71fc1763647025ff1b6f4dac448c
soniabronner <148773001+soniabronner@users.noreply.github.com>
2026-01-03 14:58:45 +0100

    Update runmodel_utils.py

--- src/utils/runmodel_utils.py
+++ src/utils/runmodel_utils.py
@@ -1,4 +1,4 @@
-from utils.evaluation_utils import evaluate_predictions
+from utils.eval_utils import evaluate_predictions
 
 def run_ml_model(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
