cd8c4dafe9cce634013632c4342cb03435a7144d
soniabronner <148773001+soniabronner@users.noreply.github.com>
2026-01-09 16:41:34 +0100

    add mae in  eval_utils.py

--- src/utils/eval_utils.py
+++ src/utils/eval_utils.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, mean_absolute_error
 
 def evaluate_predictions(y_true, y_pred):
     return {
         "accuracy": accuracy_score(y_true, y_pred),
-        "macro_f1": f1_score(y_true, y_pred, average="macro")
+        "macro_f1": f1_score(y_true, y_pred, average="macro"),
+        "mae": mean_absolute_error(y_true, y_pred)
     }
