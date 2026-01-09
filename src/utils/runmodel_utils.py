b2ee30a6c1ddb4b8951b39395f28358c41b20f6a
soniabronner <148773001+soniabronner@users.noreply.github.com>
2026-01-09 17:13:31 +0100

    Delete src/utils/runmodel_utils.py

--- src/utils/runmodel_utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from utils.eval_utils import evaluate_predictions
-
-def run_ml_model(model, X_train, X_test, y_train, y_test):
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    return evaluate_predictions(y_test, y_pred)
